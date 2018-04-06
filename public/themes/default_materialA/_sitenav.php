6aec661cbb448843300051e8aafd9ea7cf33d5d5
john.niconi@puma.com <john.niconi@puma.com>
2018-04-06 22:41:27 +0800

    FIxed theme

--- public/themes/default_materialA/_sitenav.php
+++ public/themes/default_materialA/_sitenav.php
@@ -7,7 +7,7 @@
 <a class="navbar-brand"><?php e(class_exists('Settings_lib') ? settings_item('site.title') : 'Bonfire'); ?></a>
 </div>
 <div class="collapse navbar-collapse" id="navbar-collapse">
-                <ul class="nav navbar-nav navbar-right">
+    <ul class="nav navbar-nav navbar-right">
         <li <?php echo check_class('home'); ?>><a href="<?php echo site_url(); ?>"><?php e(lang('bf_home')); ?></a></li>
         <?php if (empty($current_user)) : ?>
         <li><a href="<?php echo site_url(LOGIN_URL); ?>">Sign In</a></li>
